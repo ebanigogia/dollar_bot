dc7c41397a6b224426d4f18ca2fdafd0c699467f
shrishty19 <143622818+shrishty19@users.noreply.github.com>
2023-10-17 19:16:14 -0400

    Expense category selection added

--- code/expense.py
+++ code/expense.py
@@ -29,3 +29,17 @@ def select_expense_category(message, bot):
         markup.add(c)
     msg = bot.send_message(message.chat.id, "Select Category for expense:", reply_markup=markup)
     bot.register_next_step_handler(msg, expense_category_selected, bot)
+
+def expense_category_selected(message, bot):
+    try:
+        chat_id = message.chat.id
+        selected_category = message
+        if selected_category not in helper.getSpendCategories():
+            bot.send_message(chat_id, "Invalid", reply_markup=telebot.types.ReplyKeyboardRemove())
+            raise Exception(f'Sorry, I don\'t recognize this category "{selected_category}"!')
+
+        markup = telebot.types.ReplyKeyboardRemove()
+        msg = bot.send_message(chat_id, f"How much did you spend on {selected_category}?", reply_markup=markup)
+        bot.register_next_step_handler(msg, record_expense, selected_category, bot)
+    except Exception as e:
+        bot.reply_to(message, "Oh no! " + str(e))
