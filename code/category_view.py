9b2c2e49ae146cd75c7554b5216a257740d428ea
Rhythm Jagota <rythmjagota2@gmail.com>
2023-10-18 13:01:38 -0400

    printing list of categories

--- code/category_view.py
+++ code/category_view.py
@@ -20,27 +20,3 @@ def run(message, bot):
     else:
         message = "No categories available."
     bot.send_message(chat_id, message)
-    """
-    try:
-        print("here")
-        chat_id = message.chat.id
-        if helper.spend_categories(chat_id):
-            display_categories(message, bot)
-        else:
-            raise Exception(
-                "There are no categories available"
-            )
-    except Exception as e:
-        helper.throw_exception(e, message, bot, logging)
-        """
-"""
-def display_categories(message, bot):
-    display_overall_budget(message, bot): It takes 2 arguments for processing -
-    message which is the message from the user, and bot which is the telegram bot
-    object from the run(message, bot): in the same file. It gets the budget for the
-    user based on their chat ID using the helper module and returns the same through the bot to the Telegram UI.
-    chat_id = message.chat.id
-    for c in helper.getSpendCategories():
-        markup.add(c)
-    bot.send_message(chat_id, markup)
-    """
