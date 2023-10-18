445922d0e9d8987e61fd7bf404e7c237586ece02
Rhythm Jagota <rhythm.jagota@indiamart.com>
2023-10-17 23:59:07 -0400

    test

--- code/code.py
+++ code/code.py
@@ -20,10 +20,10 @@ configs = Properties()
 
 with open("user.properties", "rb") as read_prop:
     configs.load(read_prop)
+bot=telebot.TeleBot('6677101969:AAEQdPtnq_HMHdeP3OFGMg4LFXF_ArzTv5A')
+#api_token = str(configs.get("api_token").data)
 
-api_token = str(configs.get("api_token").data)
-
-bot = telebot.TeleBot(api_token)
+#bot = telebot.TeleBot(api_token)
 
 telebot.logger.setLevel(logging.INFO)
 
@@ -143,7 +143,7 @@ def start_and_menu_command(m):
 
 
 # defines how the /new command has to be handled/processed
-@bot.message_handler(commands=["add"])
+@bot.message_handler(commands=["category"])
 def command_add(message):
     """
     command_add(message) Takes 1 argument message which contains the message from
